93b10b1666a18ca0bc5908cbaa3c45d6bcc6a671
Elton Fonseca <elton869@gmail.com>
2024-05-02 18:07:18 -0300

    04 - Adicionando contexto aos logs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Context;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,18 @@ Route::get('/', function () {
     
     $numero = 1;
 
-    // Log::debug("O valor da variável número é " . $numero);
+    Context::add('usuario_logado', 'João');
+    Context::addHidden('chave_api', '47283748923749283749');
+
+    Log::debug("Realizando a importação dos registros do XML", [
+        'codigo_produto' => $numero
+    ]);
+
+    Context::forget('usuario_logado');
+
+    echo Context::getHidden('chave_api');
+
+
     // Log::info('O usuário acesso o recurso X');
     // Log::notice('A API integrada nesse sistema terá uma nova versão em breve');
     // Log::warning('Essa função será desabilitada na próxima versão');
@@ -16,7 +28,9 @@ Route::get('/', function () {
     // log::alert('O serviço de consulta de clientes está offline');
     // log::emergency('O banco de dados está offline');
 
-    logger()->alert('teste 123');
+    logger()->alert('teste 123', [
+        'teste' => '1234'
+    ]);
 
-    echo "final do método";
+    echo "<br>final do método";
 });
